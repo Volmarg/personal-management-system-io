358520a7f9e11f6f4e7bd263ded9f5527ecfd154
Volmarg <dwlodarczyk12@gmail.com>
2021-04-04 13:03:34 +0200

    Add webpack config to allow compiling assets

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('volt-theme', './assets/volt-theme.js')
     .addEntry('app', './assets/app.js')
-    .addEntry('external-libs', './assets/external-libs.js')
-    //.addEntry('page1', './assets/page1.js')
-    //.addEntry('page2', './assets/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -69,7 +65,6 @@ Encore
     .enableTypeScriptLoader(function (typeScriptConfigOptions) {
         typeScriptConfigOptions.transpileOnly = true;
         typeScriptConfigOptions.configFile    = 'tsconfig.json';
-    })
-;
+    });
 
 module.exports = Encore.getWebpackConfig();
